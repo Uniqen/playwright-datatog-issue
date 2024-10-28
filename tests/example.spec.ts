cc1223b5008843dd5b9039995b5f1ca1f44d2a65
Magnus Dalin <magnus.dalin@axis.com>
2024-10-28 14:18:15 +0100

    add search parameter example

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,12 +1,24 @@
 import { test, expect } from '@playwright/test';
 import tracer from 'dd-trace';
 
-tracer.init();
+tracer.init({
+  env: 'stage',
+  service: 'dementors_test',
+});
 
 test('has title', async ({ page }) => {
   const span = tracer.startSpan('MyTestSpan');
-  console.log('test traceId:', span.context().toTraceId());
-  await page.goto('https://playwright.dev/');
+
+  const searchParams = new URLSearchParams({
+    traceparent: span.context().toTraceparent(),
+  });
+
+  /**
+   * The application uses the traceparent as parent for all started OpenTelemetry spans
+   * NOTE: Today we start OpenTelemetry spans in our tests and propagate traceparent as search parameter.
+   * Unsure if dd-trace traceparent and OpenTelemetry traceparent is compatible with eachother?
+   */
+  await page.goto(`https://playwright.dev/?${searchParams}`);
   span.finish();
   await page.waitForTimeout(10_000);
 
